9d52d86c86b0e4247f925456a5fa07575f1013a7
marharita08 <rozgon.margarita08@gmail.com>
2024-12-11 15:16:10 +0200

    add snowfall background

---
+++ src/components/snowfall/snowfall.tsx
@@ -0,0 +1,16 @@
+import { Snowflake } from "../snowflake/snowflake";
+import styles from "./snowfall.module.css";
+
+const Snowfall: React.FC = () => {
+  const snowflakes = Array.from({ length: 100 });
+
+  return (
+    <div className={styles.container}>
+      {snowflakes.map((_, index) => (
+        <Snowflake key={index} delay={Math.random() * 10000} />
+      ))}
+    </div>
+  );
+};
+
+export { Snowfall };
